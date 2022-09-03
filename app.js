c3b42192eabecb45d0c04b3376f97a328d2143d2
neil <mallarineil9@gmail.com>
2022-09-03 06:02:35 -0400

    Add a function that displays the book info

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', () => displayBook())
+
 const addBookBtn = document.querySelector('.add-book-btn');
 const bookDialog = document.querySelector('#dialog-box');
 const submitBookBtn = bookDialog.querySelector('.submit-book-btn');
@@ -12,7 +14,14 @@ addBookBtn.addEventListener('click', () => {
     }
 });
 
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'The Giving Tree',
+        author: 'Shel Silverstein',
+        pages: '64',
+        status: true,
+    },
+];
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -21,20 +30,61 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
-submitBookBtn.addEventListener('click', () => addBookToLibrary());
+let book;
+let bookInLibrary = true;
 
-function addBookToLibrary() {
-    const book = new Book(bookTitleInput.value, bookAuthorInput.value,
-        bookPagesInput.value, bookStatusInput.checked);
+submitBookBtn.addEventListener('click', () => addBookToLibrary(bookTitleInput.value,
+    bookAuthorInput.value, bookPagesInput.value, bookStatusInput.checked));
 
+function addBookToLibrary(title, author, pages, status) {
+    book = new Book(title, author, pages, status);
     myLibrary.push(book);
 
     for (const property in book) {
         if (book[[property]] === '') {
             myLibrary.pop();
+            bookInLibrary = false;
             alert('Please fill all the fields');
             break;
         }
     }
 
+    displayBook();
+    return book;
+}
+
+function displayBook() {
+    const tbody = document.querySelector('tbody');
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr)
+
+    /* prevent the function from displaying the book IF the book wasn't stored in
+    the myLibrary array */
+    if (bookInLibrary === true) {
+        for (const property in myLibrary[myLibrary.length - 1]) {
+            const td = document.createElement('td');
+
+            if (property === 'status') {
+                const statusButton = document.createElement('button');
+                statusButton.classList.add('book-status-btn');
+                td.appendChild(statusButton);
+
+                checkBookStatus(myLibrary[myLibrary.length - 1][[property]]);
+
+                function checkBookStatus(status) {
+                    switch (status) {
+                        case true:
+                            statusButton.textContent = 'Read';
+                            break;
+                        case false:
+                            statusButton.textContent = 'Want to Read';
+                    }
+                }
+            } else {
+                td.textContent = myLibrary[myLibrary.length - 1][property];
+            }
+
+            tr.appendChild(td);
+        }
+    }
 }
