2651c10bd67495f4950a5fd1a3a1cc0c52aae49a
neil <mallarineil9@gmail.com>
2022-10-05 03:55:27 -0400

    Refactor codebase

--- app.js
+++ app.js
@@ -1,151 +1,174 @@
-const addBookBtn = document.querySelector('#add-book-btn');
-const bookDialog = document.querySelector('#dialog-box');
-const submitBookBtn = bookDialog.querySelector('#submit-book-btn');
-
-addBookBtn.addEventListener('click', () => {
-    if (typeof bookDialog.showModal === 'function') {
-        bookDialog.showModal();
-    }
+const addBookButton = document.querySelector('#add-book-button');
+addBookButton.addEventListener('click', () => {
+    const bookFormDialogBox = document.querySelector('#book-form-dialog-box');
+    bookFormDialogBox.showModal();
 });
 
-let myLibrary = [];
-let book = new Book('The Giving Tree', 'Shel Silverstein', '64', true);
-myLibrary.push(book);
+const bookTitleInput = document.querySelector('#book-title');
+const bookAuthorInput = document.querySelector('#book-author');
+const bookPagesInput = document.querySelector('#book-pages');
+const bookStatusInput = document.querySelector('#book-status');
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+class Book {
+    constructor(title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+
+    addBookInLibrary() {
+        newBook = new Book(bookTitleInput.value, bookAuthorInput.value,
+            bookPagesInput.value, bookStatusInput.checked);
+        myLibrary.push(newBook);
+        return isBookInLibrary = true;
+    }
 }
 
-submitBookBtn.addEventListener('click', () => displayBook(false));
+let newBook = new Book('The Giving Tree', 'Shel Silverstein', '64',
+    /* checked = */ true);
+let myLibrary = [];
+myLibrary.push(newBook);
+let isBookInLibrary = true;
 
-const form = bookDialog.querySelector('form');
-const bookTitleInput = bookDialog.querySelector('#book-title');
-const bookAuthorInput = bookDialog.querySelector('#book-author');
-const bookPagesInput = bookDialog.querySelector('#book-pages');
-const bookStatusInput = bookDialog.querySelector('#book-status');
+const displayController = {
+    form: document.querySelector('form'),
 
-function displayBook(isBookInLibrary) {
-    const errorMessageReference = document.querySelector('#error-message');
+    removeErrorMessage: function (errorMessage) {
+        if (errorMessage) {
+            errorMessage.remove();
 
-    if (errorMessageReference !== null) {
-        /* revert the button to its initial state so that the form will be
-            close automatically when the inputted book details is not
-            a duplicate */
-        submitBookBtn.setAttribute('type', 'submit');
-        errorMessageReference.remove();
-    }
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (bookTitleInput.value === myLibrary[i].title &&
-            bookAuthorInput.value === myLibrary[i].author) {
-            /* to prevent form from automatically closing when the submit
-                button was clicked */
-            submitBookBtn.setAttribute('type', 'button');
-
-            const errorMessage = document.createElement('p');
-            errorMessage.setAttribute('id', 'error-message');
-            errorMessage.textContent = 'This book already exists in your library';
-            form.appendChild(errorMessage);
-
-            obj = {};
-            return isBookInLibrary = true;
+            /* revert the button to its initial state so that the form will be
+            close automatically */
+            submitBookButton.setAttribute('type', 'submit');
         }
-    }
+    },
 
-    if (isBookInLibrary === false) {
-        // Immediately invoke function
-        (function (title, author, pages, status) {
-            book = new Book(title, author, pages, status);
-            myLibrary.push(book);
+    checkDuplicate: function () {
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (bookTitleInput.value === myLibrary[i].title &&
+                bookAuthorInput.value === myLibrary[i].author) {
+                /* For some reason, this prevents the form from closing
+                automatically when the submit button was clicked and when the
+                inputted book already exist in the library */
+                submitBookButton.setAttribute('type', 'button');
 
-            for (const property in book) {
-                if (book[property] === '') {
-                    myLibrary.pop();
-                    return book = {};
-                }
+                const errorMessage = document.createElement('p');
+                errorMessage.setAttribute('id', 'error-message');
+                errorMessage.textContent = 'This book already exists in your library';
+                this.form.appendChild(errorMessage);
+
+                newBook = null;
+                isBookInLibrary = false;
+
+                return newBook, isBookInLibrary;
             }
+        }
+    },
 
-            return book;
-        })(bookTitleInput.value, bookAuthorInput.value,
-            bookPagesInput.value, bookStatusInput.checked);
-    }
+    displayBook: function () {
+        if (isBookInLibrary) {
+            const tbody = document.querySelector('tbody');
+            const tr = document.createElement('tr');
+            // this will be use to tie the table's elements into the myLibrary array
+            tr.setAttribute('data-index', myLibrary.length - 1);
+            tbody.appendChild(tr)
 
-    if (!(Object.keys(book).length === 0)) {
-        let removeBookBtn;
-        let bookStatusBtn;
+            for (const property in newBook) {
+                const td = document.createElement('td');
 
-        const tbody = document.querySelector('tbody');
-        const tr = document.createElement('tr');
-        /* Associating the DOM element with the actual book objects so that I could
-        manipulate */
-        tr.setAttribute('data-array-element', `${myLibrary.length - 1}`);
-        tbody.appendChild(tr)
+                switch (property) {
+                    case 'title':
+                    case 'author':
+                    case 'pages':
+                        td.textContent = newBook[property];
+                        break;
+                    case 'status':
+                        td.classList.add('td-status');
+                        const bookStatusButton = document.createElement('button');
+                        bookStatusButton.addEventListener('click', (event) => {
+                            displayController.toggleBookStatusButton(event);
+                        });
+                        bookStatusButton.classList.add('book-status-button');
+                        td.appendChild(bookStatusButton);
 
-        for (const property in book) {
-            const td = document.createElement('td');
-
-            switch (property) {
-                case 'title':
-                case 'author':
-                case 'pages':
-                    td.textContent = book[property];
-                    break;
-                case 'status':
-                    td.classList.add('td-status');
-                    bookStatusBtn = document.createElement('button');
-                    bookStatusBtn.classList.add('book-status-btn');
-                    td.appendChild(bookStatusBtn);
-
-                    (function (status) {
-                        switch (status) {
+                        switch (newBook.status) {
                             case true:
-                                bookStatusBtn.classList.add('read-status');
-                                bookStatusBtn.classList.remove('want-to-read-status');
-                                return bookStatusBtn.textContent = 'Read';
+                                bookStatusButton.classList.add('read-status');
+                                bookStatusButton.textContent = 'Read';
+                                break;
                             case false:
-                                bookStatusBtn.classList.add('want-to-read-status');
-                                bookStatusBtn.classList.remove('read-status');
-                                return bookStatusBtn.textContent = 'Want to Read';
+                                bookStatusButton.classList.add('want-to-read-status');
+                                bookStatusButton.textContent = 'Want to Read';
                         }
-                    })(book.status);
 
-                    removeBookBtn = document.createElement('button');
-                    removeBookBtn.classList.add('remove-book-btn');
-                    removeBookBtn.textContent = 'Remove'
-                    td.appendChild(removeBookBtn);
+                        const removeBookButton = document.createElement('button');
+                        removeBookButton.addEventListener('click', (event) => {
+                            displayController.removeBook(event);
+                        });
+                        removeBookButton.classList.add('remove-book-button');
+                        removeBookButton.textContent = 'Remove'
+                        td.appendChild(removeBookButton);
+                }
+
+                tr.appendChild(td);
             }
+        }
+    },
 
-            tr.appendChild(td);
+    toggleBookStatusButton: function (event) {
+        switch (event.target.textContent) {
+            case 'Read':
+                event.target.classList.remove('read-status');
+                event.target.classList.add('want-to-read-status');
+                event.target.textContent = 'Want to Read';
+                break;
+            case 'Want to Read':
+                event.target.classList.remove('want-to-read-status');
+                event.target.classList.add('read-status');
+                event.target.textContent = 'Read';
         }
 
-        const bookArrayIndex = parseInt(tr.getAttribute('data-array-element'));
+        /* Since the data-index attribute is not a direct attribute of the book
+        status button, I have to traverse the DOM upward (in this case, using
+        closest() method) to retrieve the value of the said attribute */
+        const bookIndex = parseInt(event.target.closest('[data-index]')
+            .dataset.index);
 
-        bookStatusBtn.addEventListener('click', () => {
-            switch (bookStatusBtn.textContent) {
-                case 'Read':
-                    bookStatusBtn.classList.add('want-to-read-status');
-                    bookStatusBtn.classList.remove('read-status');
-                    bookStatusBtn.textContent = 'Want to Read';
-                    break;
-                case 'Want to Read':
-                    bookStatusBtn.classList.add('read-status');
-                    bookStatusBtn.classList.remove('want-to-read-status');
-                    bookStatusBtn.textContent = 'Read';
-            }
+        myLibrary[bookIndex].status = !myLibrary[bookIndex].status;
+    },
 
-            myLibrary[bookArrayIndex].status = !myLibrary[bookArrayIndex].status;
-        });
+    removeBook: function (event) {
+        const bookIndex = parseInt(event.target.closest('[data-index]')
+            .dataset.index);
 
-        removeBookBtn.addEventListener('click', () => {
-            myLibrary.splice(bookArrayIndex, 1);
+        myLibrary.splice(bookIndex, 1);
 
-            const book = document.querySelector(`[data-array-element='${bookArrayIndex}']`);
-            book.remove();
-        });
+        const bookInfo = document.querySelector(`[data-index='${bookIndex}']`);
+        bookInfo.remove();
+    },
+}
+
+/* Placing this method invocation right below the submitBookButton below
+will result in TypeError. Keep this above the submitBookButton */
+displayController.displayBook();
+
+const submitBookButton = document.querySelector('#submit-book-button');
+submitBookButton.addEventListener('click', () => {
+    displayController.removeErrorMessage(document
+        .querySelector('#error-message'));
+    displayController.checkDuplicate();
+
+    /* You might be wondering why I place the if statement here instead of
+    putting it inside the addBookInLibrary method. That's because there are
+    instance where the value of the newBook is null (i.e. when the inputted
+    book is a duplicate). If that instance occurs and there's no if statement
+    below, the newBook object will IMMEDIATELY try to access the
+    addBookInLibrary method (which doesn't exist if the value of the newBook
+    is null), which in turns will logs a TypeError error message. */
+    if (newBook) {
+        newBook.addBookInLibrary();
     }
-};
 
-displayBook(true);
+    displayController.displayBook();
+});
